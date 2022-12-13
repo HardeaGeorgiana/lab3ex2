41f94e4cd2150138d180028606c5dad73a358b2f
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-12-13 22:41:10 +0200

    Add project files.

---
+++ lab3ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lab3ex2
+{
+    /*  L3 E2 Scrieti o functie care va determina daca un numar este sau nu numar prim.
+              Apelati-o si afisati-i rezultatul
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar pentru a afla daca acesta este prim sau nu");
+            int number = int.Parse(Console.ReadLine());
+            NumarPrim(number);
+
+        }
+        static void NumarPrim (int number)
+        {
+            if (number <= 1) 
+            {
+                Console.WriteLine("Introduceti un numar mai mare decat 1");
+                return;
+            }
+
+            for(int i = 2; i <= number/2; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine("Numarul nu este prim");
+                    return;
+                }
+            }
+            Console.WriteLine("Numarul este prim");
+        }
+
+    }
+}
